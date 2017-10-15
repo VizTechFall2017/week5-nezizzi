695471838f99aa496b77058f84dc684c7b7f971e
nezizzi <zizzi.n@husky.neu.edu>
2017-10-14 23:33:54 -0400

    done for now

--- PS01/index.js
+++ PS01/index.js
@@ -6,32 +6,60 @@ var current;
 var former;
 clicked=true;
 
+
+var axislabel = ["None", "Diploma from Dance School", "Diploma from Performing Arts School", "Bachelor's Degree", " Advanced Diploma from Dance School", "Advanced Diploma from Performing Arts School","Graduate Degree"];
+
 //axes
 //var ScaleX = d3.scalePoint().domain(["None", "Diploma from Dance School", "Diploma from Performing Arts School", "Bachelor's Degree", " Advanced Diploma from Dance School", "Advanced Diploma from Performing Arts School","Graduate Degree"]).range([0, 800]);
 var ScaleX = d3.scalePoint().domain(["1", "2", "3", "4", "5", "6","7"]).range([0, 800]);
 var ScaleY = d3.scaleLinear().domain([0,30]).range([400, 0]);
 svg.append("g")
     .attr('transform','translate(0,400)')
-    .call(d3.axisBottom(ScaleX))
+    .call(d3.axisBottom(ScaleX));
 svg.append("g")
     .call(d3.axisLeft(ScaleY));
 
-axislabel = ["None", "Diploma from Dance School", "Diploma from Performing Arts School", "Bachelor's Degree", " Advanced Diploma from Dance School", "Advanced Diploma from Performing Arts School","Graduate Degree"]
+//could not figure out how to get the axe labels to be different than the values need in order to match data to it :(
+//also couldn't figure out how to use a function to get this from the array and match it to the tick marks, so i did it the LONG way
+svg.append('text')
+    .text('None')
+    .style("font", "14px times")
+    .attr('transform','translate(0,425)rotate(90)');
+svg.append('text')
+    .text('Diploma from Dance School')
+    .style("font", "14px times")
+    .attr('transform','translate(125,425)rotate(90)');
+svg.append('text')
+    .text("Diploma from Performing Arts School")
+    .style("font", "14px times")
+    .attr('transform','translate(265,425)rotate(90)');
+svg.append('text')
+    .text("Bachelor's Degree")
+    .style("font", "14px times")
+    .attr('transform','translate(385,425)rotate(90)');
+svg.append('text')
+    .text('Advanced Diploma from Dance School')
+    .style("font", "14px times")
+    .attr('transform','translate(525,425)rotate(90)');
+svg.append('text')
+    .text('Advanced Diploma from Performing Arts School')
+    .style("font", "14px times")
+    .attr('transform','translate(665,425)rotate(90)');
+svg.append('text')
+    .text('Graduate Degree')
+    .style("font", "14px times")
+    .attr('transform','translate(800,425)rotate(90)');
 
-/*svg.append('text')
-    .text(axislabel)
-    .attr('transform','translate(0,450)')
-    .attr("text-anchor", "axisBottom");*/
 
 
 svg.append('text')
     .text('DANCER EDUCATION LEVELS')
-    .attr('transform','translate(500, -20)')
+    .attr('transform','translate(350, -20)')
     .style('text-anchor','middle');
 
 svg.append('text')
     .text('Highest Dance Education Level')
-    .attr('transform','translate(250, 600)');
+    .attr('transform','translate(300, 600)');
 
 svg.append('text')
     .text('Age')
@@ -84,6 +112,8 @@ function drawPoints(pointData){
 
     svg.selectAll('.a_beg')
         .data(pointData)
+        .transition()
+        .ease(d3.easeSin)
         .attr('cx',function(d){   //look up values for all the attributes that might have changed, and draw the new circles
             return ScaleX(d.A6QUALS1);
         })
@@ -93,6 +123,8 @@ function drawPoints(pointData){
 
     svg.selectAll('.a_prof')
         .data(pointData)
+        .transition()
+        .ease(d3.easeSin)
         .attr('cx',function(d){
             return ScaleX(d.A6QUALS1);
         })
